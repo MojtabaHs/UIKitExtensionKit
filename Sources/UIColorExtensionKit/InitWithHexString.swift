73aaca70c4e2597e8c401020a8875b04859c0783
Seyed Mojtaba Hosseini Zeidabadi <chenzook@gmail.com>
2021-04-07 14:46:10 +0430

    Typo fixed.

--- Sources/UIColorExtensionKit/InitWithHexString.swift
+++ Sources/UIColorExtensionKit/InitWithHexString.swift
@@ -24,7 +24,6 @@ public extension UIColor {
     /// - Alpha value (optional)
     convenience init?(hex: String) {
         let r, g, b, a: CGFloat
-
         var hexColor = hex
 
         // Remove the prefix it appears.
@@ -33,43 +32,42 @@ public extension UIColor {
             hexColor = String(hex[start...])
         }
 
-            switch hexColor.count {
-            case 8: // Contains alpha
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
+        switch hexColor.count {
+        case 8: // Contains alpha
+            let scanner = Scanner(string: hexColor)
+            var hexNumber: UInt64 = 0
 
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
+            if scanner.scanHexInt64(&hexNumber) {
+                r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                a = CGFloat(hexNumber & 0x000000ff) / 255
 
-                    self.init(red: r, green: g, blue: b, alpha: a)
-                    return
-                } else {
-                    assertionFailure("Can not scan Hex from the number")
-                }
-            case 6: // Alpha 100%
-                let scanner = Scanner(string: hexColor + "ff")
-                var hexNumber: UInt64 = 0
-
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
-
-                    self.init(red: r, green: g, blue: b, alpha: a)
-                    return
-                } else {
-                    assertionFailure("Can not scan Hex from the number")
-                }
-
-            default:
-                assertionFailure("Invalid Hex Count")
+                self.init(red: r, green: g, blue: b, alpha: a)
+                return
+            } else {
+                assertionFailure("Can not scan Hex from the number")
             }
-        }
 
+        case 6: // Alpha 100%
+            let scanner = Scanner(string: hexColor + "ff")
+            var hexNumber: UInt64 = 0
+
+            if scanner.scanHexInt64(&hexNumber) {
+                r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                self.init(red: r, green: g, blue: b, alpha: a)
+                return
+            } else {
+                assertionFailure("Can not scan Hex from the number")
+            }
+
+        default:
+            assertionFailure("Invalid Hex Count")
+        }
         return nil
     }
 }
